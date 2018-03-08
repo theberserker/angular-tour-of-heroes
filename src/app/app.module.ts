c326183a18bf03871b497d0c13763f6fd0197cfc
Andrej Spilak <aspilak@pixi.eu>
2018-03-08 08:28:54 +0100

    6. Services - before observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
 
 
 @NgModule({
@@ -15,7 +16,9 @@ import { HeroesComponent } from './heroes/heroes.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  
+  // The providers array tells Angular to create a single, shared instance of HeroService and inject into any class that asks for it.
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +9,19 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
-  
+  heroes: Hero[]
+
   selectedHero: Hero;  
 
-  constructor() { }
+  constructor(private heroService: HeroService) { 
+    // While you could call getHeroes() in the constructor, that's not the best practice.
+    // Reserve the constructor for simple initialization such as wiring constructor parameters to properties.
+    // he constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
+    this.heroService = this.heroService;
+  }
 
   ngOnInit() {
+    this.heroes = this.heroService.getHeroes();
   }
   
   onSelect(hero: Hero): void {
